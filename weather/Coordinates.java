959cfd77b30f9f28c202646c290a2dd1a79a0096
Deathshadowown <shanervanniekerk@gmail.com>
2020-08-11 16:03:43 +0200

    added class for Coordinates

--- weather/Coordinates.java
+++ weather/Coordinates.java
@@ -11,3 +11,26 @@
 /* ************************************************************************** */
 
 package avaj-launcher.weather;
+
+public class Coordinates{
+    private int longitude;
+    private int latitude;
+    private int height;
+
+    public int Coordinates(longitude: int, latitude: int, height: int)
+    {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.height = height;
+    }
+
+    public int getLongitude(){
+        return this.longitude;
+    }
+    public int getLatitude(){
+        return this.latitude;
+    }
+    public int getHeight(){
+        return this.height;
+    }
+}
