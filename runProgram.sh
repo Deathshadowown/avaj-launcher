f125e8752b06907b144dcefdf46d63485674c5ed
Deathshadowown <shanervanniekerk@gmail.com>
2020-08-24 10:57:54 +0200

    added sh file to run and scenario file

---
+++ runProgram.sh
@@ -0,0 +1,10 @@
+rm -f simulator/vehicles/*.class
+rm -f simulator/*.class
+rm -f weather/*.class
+rm -f simulation.txt
+javac simulator/Simulator.java
+java simulator.Simulator scenario.txt
+rm -f simulator/vehicles/*.class
+rm -f simulator/*.class
+rm -f weather/*.class
+rm -f simulation.txt
